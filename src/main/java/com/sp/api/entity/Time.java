682300587eb0bc4cd2ac0d4c9c97dc3c091b6790
l524l <65085630+l524l@users.noreply.github.com>
2020-12-24 15:38:48 +0300

    У класса Time удалены setters

--- src/main/java/com/sp/api/entity/Time.java
+++ src/main/java/com/sp/api/entity/Time.java
@@ -6,40 +6,23 @@ package com.sp.api.entity;
 public class Time {
     private TimeOfDay time;
     private int ticks;
-    private String formated;
 
-    public Time() {
-    }
-
-    public Time(TimeOfDay time, int ticks, String formated) {
+    public Time(TimeOfDay time, int ticks) {
         this.time = time;
         this.ticks = ticks;
-        this.formated = formated;
     }
 
     public TimeOfDay getTime() {
         return time;
     }
 
-    public void setTime(TimeOfDay time) {
-        this.time = time;
-    }
-
     public int getTicks() {
         return ticks;
     }
 
-    public void setTicks(int ticks) {
-        this.ticks = ticks;
-    }
-
     public String getFormated() {
-        int s = (int) Math.floor(ticks / 1000) + 6;
+        int s = (int) Math.floor(ticks / 1000);
         return String.format("%02d", s) + ":" + String.format("%02d", Math.round((ticks % 1000)*0.06));
-    }
-
-    public void setFormated(String formated) {
-        this.formated = formated;
     }
 
     @Override
@@ -50,15 +33,14 @@ public class Time {
         Time time = (Time) o;
 
         if (ticks != time.ticks) return false;
-        if (this.time != time.time) return false;
-        return formated.equals(time.formated);
+        return this.time == time.time;
     }
 
     @Override
     public int hashCode() {
         int result = time.hashCode();
         result = 31 * result + ticks;
-        result = 31 * result + formated.hashCode();
+        result = 31 * result;
         return result;
     }
 }
