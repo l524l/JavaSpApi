d2912eea3cc3c5184e1ebd7bbd81c3e9beb72e5b
l524l <65085630+l524l@users.noreply.github.com>
2020-12-26 15:12:11 +0300

    У класса LastChatMessages появились методы по поиску сообщений по данным отправителя

--- src/main/java/com/sp/api/entity/LastChatMessages.java
+++ src/main/java/com/sp/api/entity/LastChatMessages.java
@@ -1,7 +1,9 @@
 package com.sp.api.entity;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @Author Pershin Andrew
@@ -13,6 +15,7 @@ public class LastChatMessages {
     public LastChatMessages(List<Message> messages) {
         this.messages = (Message[]) messages.toArray();
     }
+
     public LastChatMessages(Message[] messages) {
         this.messages = messages;
     }
@@ -22,6 +25,22 @@ public class LastChatMessages {
     }
 
     public Message[] getMessages() {
+        return messages;
+    }
+
+    public List<Message> findMessagesBySenderName(String name){
+        ArrayList<Message> messages = new ArrayList<>();
+        getMessagesAsList().listIterator().forEachRemaining((x) -> {
+            if (x.getSender().getNickname().equals(name)) messages.add(x);
+        });
+        return messages;
+    }
+
+    public List<Message> findMessagesBySenderUUID(UUID uuid){
+        ArrayList<Message> messages = new ArrayList<>();
+        getMessagesAsList().listIterator().forEachRemaining((x) -> {
+            if (x.getSender().getUuid().equals(uuid)) messages.add(x);
+        });
         return messages;
     }
 }
